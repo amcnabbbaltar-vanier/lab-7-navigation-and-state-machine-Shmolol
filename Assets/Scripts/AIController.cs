84c7249549226cd2393285844bd08ca4bef2ef39
Shmolol <karlalvarado666@gmail.com>
2025-03-26 16:29:11 -0400

    Finished up until Step 5
    Step 5 still needs  to be finished

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -3,4 +3,25 @@ using UnityEngine.AI;
 
 public class AIController : MonoBehaviour
 {
+    public StateMachine StateMachine { get; private set; }
+    public NavMeshAgent Agent { get; private set; }
+    public Animator Animator { get; private set; } // Not needed since we're not using animations
+
+    public Transform[] Waypoints;
+    public Transform Player;
+    public float SightRange = 10f;
+    public float AttackRange = 2f; // New attack range variable
+    public LayerMask PlayerLayer;
+    public StateType currentState;
+
+    void Start()
+    {
+        Agent = GetComponent<NavMeshAgent>();
+        // Animator = GetComponent<Animator>(); // Commented out since we're not using animations
+
+        StateMachine = new StateMachine();
+
+        StateMachine.AddState(new IdleState(this));
+        StateMachine.AddState(new PatrolState(this));
+    }
 }
