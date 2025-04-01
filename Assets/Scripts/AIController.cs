06278cfd7e8fd11474066e87c9736983d6934fa6
Shmolol <karlalvarado666@gmail.com>
2025-04-01 14:58:15 -0400

    Finished Lab 7

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -23,5 +23,37 @@ public class AIController : MonoBehaviour
 
         StateMachine.AddState(new IdleState(this));
         StateMachine.AddState(new PatrolState(this));
+        StateMachine.AddState(new ChaseState(this));
+        StateMachine.AddState(new AttackState(this));
+
+        StateMachine.TransitionToState(StateType.Idle);
+    }
+
+    void Update()
+    {
+        StateMachine.Update();
+        Animator.SetFloat("CharacterSpeed", Agent.velocity.magnitude);
+        currentState = StateMachine.GetCurrentStateType();
+    }
+
+    public bool CanSeePlayer()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, Player.position);
+        
+        if (distanceToPlayer <= SightRange)
+        {
+            // Optionally, add line of sight checks here using Raycast
+            return true;
+        }
+
+        return false;
+    }
+
+    // New method to check if if the AI is within attack range
+    public bool IsPlayerInAttackRange()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, Player.position);
+
+        return distanceToPlayer <= AttackRange;
     }
 }
